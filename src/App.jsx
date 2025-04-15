b572707b9dc5f54fe5506ed82bfd606d1a0df900
Ansh <anshkant090@gmail.com>
2025-04-16 02:17:06 +0530

    newly started

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button"
-import Home from "./pages/Home/Home"
+import React from 'react'
+import Home from './pages/Home/Home'
 
-
-function App() {
+const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
+    <div>
       <Home />
     </div>
   )
--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -1,16 +1,40 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { MixerHorizontalIcon } from '@radix-ui/react-icons';
+import { ScrollArea } from '@radix-ui/react-scroll-area';
 import React from 'react'
 
 const ProjectList = () => {
     return (
         <>
-            <div className='relative px-5 lg:px-0 lg-flex gap-5 justify-center bg-red-500'>
-                Project List
-
+            <div className='relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5'>
                 <section className='filterSection'>
                     Filter Section
+                    
+                    <Card className="p-5 sticky top-10">
+
+                    <div className='flex justify-between lg:w-[20rem]'>
+                        <p className='text-xl -tracking-wider'>filters</p>
+
+                        <Button variant="ghost" size="icon">
+
+                            <MixerHorizontalIcon />
+
+                        </Button>
+                    </div>
+
+                    <CardContent className="mt-5">  
+
+                        <ScrollArea className='space-y-7'>
+
+                        </ScrollArea>
+
+                    </CardContent>
+                    
+                    </Card>
                 </section>
 
-                <section className='projectListSection'>
+                <section className='projectListSection w-full lg:w-[48rem]'>
                     Project List Section
                 </section>
             </div>
