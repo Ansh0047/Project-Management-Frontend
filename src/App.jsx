b4203dbcdff846020c577a6c04d52a46829ea692
Ansh <anshkant090@gmail.com>
2025-04-15 22:36:28 +0530

    Stating Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import Home from "./pages/Home/Home"
+
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/ProjectList/ProjectList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <>
+            <div className='relative px-5 lg:px-0 lg-flex gap-5 justify-center bg-red-500'>
+                Project List
+
+                <section className='filterSection'>
+                    Filter Section
+                </section>
+
+                <section className='projectListSection'>
+                    Project List Section
+                </section>
+            </div>
+        </>
+    );
+}
+
+export default ProjectList
